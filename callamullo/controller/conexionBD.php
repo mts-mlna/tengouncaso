1882885b00182a3c88d8a48c6f861682f136d918
Bruno Ezeqiel Sánchez <3zequielbsanchez@gmail.com>
2025-07-03 23:26:55 -0300

    Delete callamullo directory
    me confundi

--- callamullo/controller/conexionBD.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    $Dirrecion = new mysqli("localhost", "root", "", "tengouncaso");
-    if ($Dirrecion->connect_error) {
-        die("Connection failed: " . $Dirrecion->connect_error);
-    }
